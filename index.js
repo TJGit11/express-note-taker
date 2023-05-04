df3ef2a7e16cd3a58b8980b03e6feb1f8ef17123
unknown <thomasjjohnson9311@gmail.com>
2023-05-03 22:53:29 -0500

    additional starter files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 // Helper method for generating unique ids
-const uuid = require("./helpers/uuid");
 
 const PORT = process.env.PORT || 3001;
 
