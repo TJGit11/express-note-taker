c41266ad7c1303225e11481a584b272070dd5f0f
unknown <thomasjjohnson9311@gmail.com>
2023-05-04 22:07:09 -0500

    api get/post test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 // Helper method for generating unique ids
-const db = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
+// const notes = require("./db/db.json");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -15,25 +17,50 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-// GET request for reviews
-app.get("/api/notes", (req, res) => {
-  res.status(200).json({ example: 1 });
-});
-
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("/api/notes", (req, res) => {
+  // res.status(200).json({ notes: 1 });
+});
+
+// app.post("/api/notes", (req, res) => {
+//   console.log(req.body);
+// });
+
 app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
   //pull data out form req.body
+  // const { id, title, text } = req.body
+  const notesData = req.body;
   //push to the db array
-  // write to file
+  if (notesData) {
+    const newNote = {
+      id: uuidv4(),
+      title,
+      text,
+    };
+
+    const noteString = JSON.stringify(newNote);
+
+    // write to file
+    fs.writeFile(`./db/${newNote}.json`, noteString, (err) =>
+      err
+        ? console.log(err)
+        : console.log(`Review for ${newNote.id} has been written to JSON file`)
+    );
+  }
 });
 
 app.delete("/api/notes", (req, res) => {
   //pull from url
   // remove item from array
   // write to file
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () =>
