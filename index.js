cb7103cf341654d1e2586d916dd2cdad7aed330e
unknown <thomasjjohnson9311@gmail.com>
2023-05-04 23:11:42 -0500

    id test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 // Helper method for generating unique ids
-const { v4: uuidv4 } = require("uuid");
+// const uuid = require("uuid");
 // const notes = require("./db/db.json");
 const fs = require("fs");
+// const util = require("util");
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -22,42 +24,39 @@ app.get("/notes", (req, res) =>
 );
 
 app.get("/api/notes", (req, res) => {
-  // res.status(200).json({ notes: 1 });
+  res.json(`${req.method} request received`);
+  fs.readFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // app.post("/api/notes", (req, res) => {
-//   console.log(req.body);
+//   console.info(`${req.method} request received to add a new note`);
+//   //pull data out form req.body
+//   // const { id, title, text } = req.body
+//   const notesData = req.body;
+//   //push to the db array
+//   if (notesData) {
+//     const newNote = {
+//       id: uuidv4(),
+//       title,
+//       text,
+//     };
+
+//     const noteString = JSON.stringify(newNote);
+
+//     // write to file
+//     fs.writeFile(`./db/${newNote}.json`, noteString, (err) =>
+//       err
+//         ? console.log(err)
+//         : console.log(`Review for ${newNote.id} has been written to JSON file`)
+//     );
+//   }
 // });
 
-app.post("/api/notes", (req, res) => {
-  console.info(`${req.method} request received to add a new note`);
-  //pull data out form req.body
-  // const { id, title, text } = req.body
-  const notesData = req.body;
-  //push to the db array
-  if (notesData) {
-    const newNote = {
-      id: uuidv4(),
-      title,
-      text,
-    };
-
-    const noteString = JSON.stringify(newNote);
-
-    // write to file
-    fs.writeFile(`./db/${newNote}.json`, noteString, (err) =>
-      err
-        ? console.log(err)
-        : console.log(`Review for ${newNote.id} has been written to JSON file`)
-    );
-  }
-});
-
-app.delete("/api/notes", (req, res) => {
-  //pull from url
-  // remove item from array
-  // write to file
-});
+// app.delete("/api/notes", (req, res) => {
+//   //pull from url
+//   // remove item from array
+//   // write to file
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
